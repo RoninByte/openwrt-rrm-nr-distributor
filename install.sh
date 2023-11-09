2b4bb3ecf253f1517237246f3cdb64b0a0f83b05
Sakurai, Paul Mikki <23254804+pdsakurai@users.noreply.github.com>
2023-11-09 15:49:41 +0800

    Change: added logs to monitor progress

--- install.sh
+++ install.sh
@@ -1,11 +1,20 @@
 #!/bin/sh
 
+readonly service_name="update_rrm_nr"
+
+function log() {
+    local text="${1:?Missing: Text}"
+
+    logger -t "$service_name" -p daemon.info "$text"
+    echo "$text"
+}
+
 function install_prerequisite_pkg() {
     local pkg_name="${1:?Missing: Package name}"
     [ $( opkg list-installed | grep -c "$pkg_name" ) -ge 1 ] && return 0
 
     opkg update
-    opkg install "$pkg_name"
+    opkg install "$pkg_name" && log "Done installing: $pkg_name"
 }
 
 function enable_and_start_service() {
@@ -13,10 +22,10 @@ function enable_and_start_service() {
 
     service "$service_name" enable
     service "$service_name" start
+    log "Done enabling and starting service: $service_name"
 }
 
 function install_service() {
-    local service_name="update_rrm_nr"
 
     service "$service_name" stop &> /dev/null
 
@@ -31,8 +40,12 @@ function install_service() {
         local dest_file="$( head -1 "$tmp_file" | cut -d' ' -f3 )"
         tail -n +2 "$tmp_file" > "$dest_file"
         chmod +x "$dest_file"
+        log "Done downloading to: $dest_file"
 
-        [ $( grep -c "$dest_file" "$backup_config_file" ) -le 0 ] && echo "$dest_file" >> "$backup_config_file"
+        [ $( grep -c "$dest_file" "$backup_config_file" ) -le 0 ] && {
+            echo "$dest_file" >> "$backup_config_file"
+            log "Added to backup list: $dest_file"
+        }
 
         echo "" > "$tmp_file"
     done
@@ -41,9 +54,7 @@ function install_service() {
     enable_and_start_service "umdns"
     enable_and_start_service "$service_name"
 
-    local log="Installation completed successfully."
-    logger -t "$service_name" -p daemon.info "$log"
-    echo "$log"
+    log "Installation completed successfully."
 }
 
 install_service
